73127ce7bb26857587730b313fe88815d43ca019
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-17 01:21:23 +1300

    feat: Created King Attack Table

--- managers/table_manager.py
+++ managers/table_manager.py
@@ -12,6 +12,7 @@ from src.constants.board_constants import (
 class TableManager:
     def __init__(self):
         self.pawn_attack_table = [[0] * SQUARES for _ in range(SIDES)]
+        self.king_attack_table = [0] * SQUARES
         self.knight_attack_table = [0] * SQUARES
 
         self.initialize()
@@ -23,10 +24,10 @@ class TableManager:
         for square in range(SQUARES):
             white_pawn_attacks = Attack.get_pawn_attacks(colors["white"], square)
             black_pawn_attacks = Attack.get_pawn_attacks(colors["black"], square)
-
+            king_attacks = Attack.get_king_attacks(square)
             knight_attacks = Attack.get_knight_attacks(square)
 
             self.pawn_attack_table[colors["white"]][square] = white_pawn_attacks
             self.pawn_attack_table[colors["black"]][square] = black_pawn_attacks
-
+            self.king_attack_table[square] = king_attacks
             self.knight_attack_table[square] = knight_attacks
