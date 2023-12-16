37bae3b64cf79a5df1a93df9ecacf0fc887db785
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-16 21:04:54 +1300

    feat: Added Bit Helper Functions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy
-
+from src.utilities.bit import Bit
 from src.utilities.console import Console
 
-Console.print_bitboard(numpy.uint64(0))
+
+bitboard = 4
+
+Console.print_bitboard(bitboard)
--- src/utilities/console.py
+++ src/utilities/console.py
@@ -1,14 +1,23 @@
-import numpy
+from src.utilities.bit import Bit
 
 from src.constants.board_constants import RANKS, FILES
 
 
 class Console:
     @staticmethod
-    def print_bitboard(bitboard: numpy.uint64):
+    def print_bitboard(bitboard):
+        print()
+
         for rank in range(RANKS):
             for file in range(FILES):
                 square = rank * 8 + file
-                print(square, end=" ")
+
+                if not file:
+                    print(f"  {8 - rank} ", end="")
+
+                print(f" {1 if Bit.get_bit(bitboard, square) else 0}", end="")
 
             print()
+
+        print("\n     a b c d e f g h\n")
+        print(f"     Bitboard: {bitboard}")
