a3ca7eefbc1326c380fc6cf1d08234c000c7bdac
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-17 11:04:11 +1300

    feat: Added Rook Slider Test

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ tmp = table_manager.bishop_attack_table
 
 # Console.print_bitboard(pawn_attacks(squares["h4"], colors["black"]))
 for i in range(64):
-    Console.print_bitboard(Attack.get_bishop_attacks(i))
+    Console.print_bitboard(Attack.get_rook_attacks(i))
 
 # Console.print_bitboard(Attack.get_bishop_attacks(squares["d4"]))
