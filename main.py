4fa45f455c2bc169bb0fc716af53620baea044fb
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-17 00:31:12 +1300

    feat: Updated Main And Board Constants

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-from src.lookup.squares import squares
-from src.lookup.colors import colors
-
+from managers.table_manager import TableManager
 from src.utilities.bit import Bit
 from src.utilities.console import Console
 
-from src.constants.board_constants import (
-    NOT_A_FILE,
-    NOT_H_FILE,
-    NOT_HG_FILE,
-    NOT_AB_FILE,
-)
+table_manager = TableManager()
 
+tmp = table_manager.pawn_attack_table
 
-def pawn_attacks(square, side):
-    attacks = bitboard = 0
-
-    bitboard = Bit.set_bit(bitboard, square)
-
-    if not side:
-        attacks = Bit.right_shift(bitboard, 7)
-    else:
-        pass
-
-    return attacks
-
-
-# bitboard = 0
-# Console.print_bitboard(bitboard)
+# Console.print_bitboard(pawn_attacks(squares["h4"], colors["black"]))
--- src/utilities/bit.py
+++ src/utilities/bit.py
@@ -16,10 +16,6 @@ class Bit:
         return x + y
 
     @staticmethod
-    def right_shift(value, offset):
-        return value >> offset
-
-    @staticmethod
     def get_bit(value, position):
         return value & (1 << position)
 
