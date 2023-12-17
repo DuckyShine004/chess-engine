23a85fcc79b2f98ee486f9778fff84590167e9fe
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-17 13:15:48 +1300

    feat: Added Conversion Wrappers

---
+++ src/wrappers/wrapper.py
@@ -0,0 +1,24 @@
+from src.constants.bit_constants import (
+    BIT_32_MASK,
+    BIT_64_MASK,
+)
+
+
+class Wrapper:
+    @staticmethod
+    def uint32(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+
+            return result & BIT_32_MASK
+
+        return wrapper
+
+    @staticmethod
+    def uint64(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+
+            return result & BIT_64_MASK
+
+        return wrapper
