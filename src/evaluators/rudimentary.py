f84455618234ab63127c91ec7534b700c6174490
Gallon Zhou <gallonzhou04@gmail.com>
2024-01-01 22:13:50 +1300

    refactor: Refactored Rudimentary Evaluator

--- src/evaluators/rudimentary.py
+++ src/evaluators/rudimentary.py
@@ -21,38 +21,48 @@ class Rudimentary:
 
         for piece in range(NUMBER_OF_BITBOARDS):
             bitboard = app.bitboard_manager.bitboards[piece]
-
-            # Loop over the pieces within a bitboard
-            while bitboard:
-                square = Bit.get_least_significant_first_bit(bitboard)
-
-                # Get the current material's score
-                score += MATERIAL_SCORES[piece]
-
-                # score positional piece scores
-                match piece:
-                    case piece if piece == PIECES["P"]:
-                        score += PAWN_SCORES[square]
-                    case piece if piece == PIECES["N"]:
-                        score += KNIGHT_SCORES[square]
-                    case piece if piece == PIECES["B"]:
-                        score += BISHOP_SCORES[square]
-                    case piece if piece == PIECES["R"]:
-                        score += ROOK_SCORES[square]
-                    case piece if piece == PIECES["K"]:
-                        score += KING_SCORES[square]
-                    case piece if piece == PIECES["p"]:
-                        score -= PAWN_SCORES[MIRROR_SCORES[square]]
-                    case piece if piece == PIECES["n"]:
-                        score -= KNIGHT_SCORES[MIRROR_SCORES[square]]
-                    case piece if piece == PIECES["b"]:
-                        score -= BISHOP_SCORES[MIRROR_SCORES[square]]
-                    case piece if piece == PIECES["r"]:
-                        score -= ROOK_SCORES[MIRROR_SCORES[square]]
-                    case piece if piece == PIECES["k"]:
-                        score -= KING_SCORES[MIRROR_SCORES[square]]
-
-                # Decrement least significant first bit
-                bitboard = Bit.pop_bit(bitboard, square)
+            score = Rudimentary.evaluate_board(bitboard, piece, score)
 
         return score if app.bitboard_manager.side == SIDES["white"] else -score
+
+    @staticmethod
+    def evaluate_board(bitboard, piece, score):
+        while bitboard:
+            square = Bit.get_least_significant_first_bit(bitboard)
+
+            score = Rudimentary.evaluate_material(piece, score)
+            score = Rudimentary.evaluate_position(square, piece, score)
+
+            bitboard = Bit.pop_bit(bitboard, square)
+
+        return score
+
+    @staticmethod
+    def evaluate_material(piece, score):
+        return score + MATERIAL_SCORES[piece]
+
+    @staticmethod
+    def evaluate_position(square, piece, score):
+        match piece:
+            case piece if piece == PIECES["P"]:
+                score += PAWN_SCORES[square]
+            case piece if piece == PIECES["N"]:
+                score += KNIGHT_SCORES[square]
+            case piece if piece == PIECES["B"]:
+                score += BISHOP_SCORES[square]
+            case piece if piece == PIECES["R"]:
+                score += ROOK_SCORES[square]
+            case piece if piece == PIECES["K"]:
+                score += KING_SCORES[square]
+            case piece if piece == PIECES["p"]:
+                score -= PAWN_SCORES[MIRROR_SCORES[square]]
+            case piece if piece == PIECES["n"]:
+                score -= KNIGHT_SCORES[MIRROR_SCORES[square]]
+            case piece if piece == PIECES["b"]:
+                score -= BISHOP_SCORES[MIRROR_SCORES[square]]
+            case piece if piece == PIECES["r"]:
+                score -= ROOK_SCORES[MIRROR_SCORES[square]]
+            case piece if piece == PIECES["k"]:
+                score -= KING_SCORES[MIRROR_SCORES[square]]
+
+        return score
